ebd4714e021522c2a43c37e30eece563615634cc
AlinaOktyabrskaya <44409263+AlinaOktyabrskaya@users.noreply.github.com>
2018-10-31 21:00:30 +0300

    Add TextArea in TextEditor class

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -5,7 +5,32 @@ import javax.swing.*;
 public class TextEditor extends JFrame {
     public TextEditor() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
         setVisible(true);
+ setLayout(null);
+        setTitle("Text Editor");
+        createTextArea();
+        setSize(new Dimension(350, 300));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+    }
+
+    private void createTextArea() {
+        JTextArea textArea = new JTextArea();
+        JScrollPane scrollPanepane = new JScrollPane(textArea);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        Container pane = getContentPane();
+        GroupLayout groupLayout = new GroupLayout(pane);
+        pane.setLayout(groupLayout);
+        groupLayout.setAutoCreateContainerGaps(true);
+        groupLayout.getAutoCreateGaps();
+
+        groupLayout.setHorizontalGroup(groupLayout.createSequentialGroup()
+                .addComponent(scrollPanepane));
+        groupLayout.setVerticalGroup(groupLayout.createSequentialGroup()
+                .addComponent(scrollPanepane));
+
+        pack();
     }
 }
