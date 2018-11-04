702b7ecf7cefb943e6554f15b3f58d8b4ff7b096
SwiftFalcon <korolyov.k@gmail.com>
2018-11-04 17:17:12 +0300

    Add createLayout and fix some issues with Eclipse IDE.

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -1,10 +1,17 @@
 package editor;
 
+import java.awt.Container;
+
 import javax.swing.*;
 
 //A simple JTextArea component
 
 public class TextEditor extends JFrame {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
     //constructor - initializing a JTextArea component 
     public TextEditor() {
         JTextArea area = new JTextArea(); //constructor of the JTextArea component.
@@ -13,7 +20,7 @@ public class TextEditor extends JFrame {
         area.setLineWrap(true); //makes the lines wrapped if they are too long to fit the text area's width.
         area.setWrapStyleWord(true); //lines will be wrapped at word boundaries—white spaces.
 
-        //createLayout(spane);
+        createLayout(spane);
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
@@ -21,5 +28,25 @@ public class TextEditor extends JFrame {
         setTitle("JTextEditor");
     }
     
+    private void createLayout(JComponent... arg) {
+
+        Container pane = getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+        gl.setAutoCreateGaps(true);
+
+        gl.setHorizontalGroup(gl.createParallelGroup()
+                .addComponent(arg[0])
+
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+
+        pack();
+    }
     
 }
