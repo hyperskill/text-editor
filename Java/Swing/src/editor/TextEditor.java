47a8a10daadce607625682b06560b877f6fec072
fsb4000 <fsb4000@yandex.ru>
2019-03-02 20:58:21 +0700

    stage three

--- Java/Swing/src/editor/TextEditor.java
+++ Java/Swing/src/editor/TextEditor.java
@@ -5,7 +5,6 @@ package editor;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -20,18 +19,44 @@ public class TextEditor extends JFrame {
     private JButton save;
     private JButton load;
     private JPanel main;
+    private JPanel buttons;
     private JScrollPane textScroll;
+    private AbstractAction saveAction;
+    private AbstractAction loadAction;
+    private AbstractAction exitAction;
+    private JMenuBar menuBar;
+    private JMenuItem loadMenuItem;
+    private JMenuItem saveMenuItem;
+    private JMenuItem exitMenuItem;
 
     public TextEditor() {
-        setupMainWindowParameters();
         createWidgets();
-        setupStyle();
+        createActions();
         setupLayout();
         setupActions();
+        setupMenu();
+        setupStyle();
+        setupMainWindowParameters();
     }
 
-    private void setupActions() {
-        save.addActionListener(new ActionListener() {
+    private void setupMenu() {
+        JMenu file = new JMenu("File");
+
+        file.add(loadMenuItem);
+        file.add(saveMenuItem);
+
+        file.addSeparator();
+
+        file.add(exitMenuItem);
+
+        menuBar.add(file);
+    }
+
+    private void createActions() {
+        saveAction = new AbstractAction() {
+            {
+                putValue(NAME, "Save");
+            }
             @Override
             public void actionPerformed(final ActionEvent e) {
                 File file = new File(fileName.getText());
@@ -43,9 +68,12 @@ public class TextEditor extends JFrame {
                     e1.printStackTrace();
                 }
             }
-        });
+        };
 
-        load.addActionListener(new ActionListener() {
+        loadAction = new AbstractAction() {
+            {
+                putValue(NAME, "Load");
+            }
             @Override
             public void actionPerformed(final ActionEvent e) {
                 byte[] encoded = null;
@@ -56,19 +84,39 @@ public class TextEditor extends JFrame {
                 }
                 textArea.setText(new String(encoded, StandardCharsets.UTF_8));
             }
-        });
+        };
+
+        exitAction = new AbstractAction() {
+            {
+                putValue(NAME, "Exit");
+            }
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                dispose();
+            }
+        };
+    }
+
+    private void setupActions() {
+        save.addActionListener(saveAction);
+        load.addActionListener(loadAction);
+
+        saveMenuItem.setAction(saveAction);
+        loadMenuItem.setAction(loadAction);
+        exitMenuItem.setAction(exitAction);
     }
 
     private void setupMainWindowParameters() {
         setTitle("Text Editor");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setJMenuBar(menuBar);
+        setSize(400,400);
         setVisible(true);
     }
 
     private void setupLayout() {
         main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
 
-        JPanel buttons = new JPanel();
         buttons.setLayout(new BoxLayout(buttons, BoxLayout.X_AXIS));
         buttons.add(fileName);
         buttons.add(Box.createRigidArea(new Dimension(12, 0)));
@@ -85,16 +133,20 @@ public class TextEditor extends JFrame {
         main.add(textPanel);
 
         add(main);
-        pack();
     }
 
     private void createWidgets() {
         main = new JPanel();
-        textArea = new JTextArea(20, 10);
+        buttons = new JPanel();
+        textArea = new JTextArea(10, 10);
         fileName = new JTextField(30);
         save = new JButton("Save");
         load = new JButton("Load");
         textScroll = new JScrollPane(textArea);
+        menuBar = new JMenuBar();
+        loadMenuItem = new JMenuItem();
+        saveMenuItem = new JMenuItem();
+        exitMenuItem = new JMenuItem();
     }
 
     private void setupStyle() {
@@ -104,9 +156,11 @@ public class TextEditor extends JFrame {
         float size = font.getSize() + 2.0f;
         textArea.setFont(font.deriveFont(size));
 
-        textScroll.setHorizontalScrollBarPolicy(
-                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-        textScroll.setVerticalScrollBarPolicy(
-                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        Dimension maxSize = fileName.getPreferredSize();
+        fileName.setMaximumSize(maxSize);
+
+        buttons.add(Box.createGlue());
+        buttons.setMaximumSize(new Dimension(buttons.getMaximumSize().width,
+                buttons.getPreferredSize().height));
     }
 }
